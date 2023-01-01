21924a3c8270f85c6192cf05118999690bb57a8e
V-I-S-H-A-L <116909259+Vishu-993@users.noreply.github.com>
2023-01-01 20:55:16 +0530

    Add files via upload

--- Plugins/bgm/bgm.js
+++ Plugins/bgm/bgm.js
@@ -1,45 +1,9 @@
-const { tlang } = require('../../lib')
-const Config = require('../../config')
-const axios = require('axios')
-const url = 'https://raw.githubusercontent.com/SamPandey001/Secktor-Plugins/main/plugins/bgm/bgm.js'
-
-const BGM_MIME_TYPE = 'audio/mpeg'
-const BGM_ENV_VAR = 'BGM'
-const BGM_CHAT_COMMAND = 'bgm'
-
-async function playBgm(citel) {
-  // Fetch the current value of the BGM environment variable
-  const { BGM } = process.env
-  if (BGM !== 'true') return
-
-  // Fetch the BGM audio file from the specified URL
-  const response = await axios.get(url, {
-    responseType: 'arraybuffer',
-  })
-  const audioBuffer = response.data
-
-  // Send the BGM audio file as a message
-  await citel.send({
-    type: 'audio',
-    originalContentUrl: url,
-    audio: {
-      contentType: BGM_MIME_TYPE,
-      buffer: audioBuffer,
-    },
-  })
+{
+"begins" : "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/And%20so%20it%20begins.mp3?raw=true",
+ "sparrow": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Jack%20Sparrow%20Images%20!%20Jack%20Sparrow%20!%20Theme.mp3?raw=true",
+ "sorry": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
+ "piano": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Bachelor%20Movie%20Piano%20Bgm%20-%20Adiye%20Bgm.mp3",
+  "first class": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/First%20Class%20-%20Arjit%20Singh%20!%20Romantic%20!%20Hindi.mp3",
+  "got you": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/I%20Got%20You%20-%20Levitating%20-%20Dua%20Lipa%20!%20English%20Song.mp3",
+  "sad": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/alan-faded.mp3"
 }
-
-module.exports = {
-  name: BGM_CHAT_COMMAND,
-  category: 'misc',
-  desc: 'Turns on/off bgm.',
-  async exec(citel, Void, args, isCreator) {
-    if (!isCreator) return citel.reply(tlang().owner)
-    if (!args[0]) return citel.reply(`Please give me option true/false to set ${tlang().greet}`)
-
-    const Heroku = require('heroku-client')
-    const heroku = new Heroku({
-      token: Config.HEROKU.API_KEY,
-    })
-    const baseUri = `/apps/${Config.HEROKU.APP_NAME}`
-    await heroku
